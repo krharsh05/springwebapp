69f216d56a74466dba1798cbdac01c40c8c6b3f8
Harsh Kumar <kr.harsh1986@gmail.com>
2020-05-06 21:32:25 +0530

    "Commit SprigwebappApplication.java"

--- src/main/java/com/harsh/springwebapp/SpringwebappApplication.java
+++ src/main/java/com/harsh/springwebapp/SpringwebappApplication.java
@@ -7,7 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringwebappApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SpringwebappApplication.class, args);
+
+		System.out.println("Hello World");
 	}
 
 }
